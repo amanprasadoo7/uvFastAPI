9f88d684d21cb3c0f50b2f3a00d4d4d64a7a0bbd
amanprasadoo7 <amanprasad5455@gmail.com>
2026-01-05 00:29:42 +0530

    added the test db file to test the db connection file

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ uv venv
 source .venv/bin/activate
 
 uv add fastapi uvicorn pydantic pydantic_settings
+
+uv run python -m test.db_test
---
+++ test/db_test.py
@@ -0,0 +1,39 @@
+import asyncio
+from app.core.config import settings
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy import text
+
+DATABASE_URL = str(settings.database_url)
+
+async def test_db():
+    engine = create_async_engine(DATABASE_URL, echo=True)
+
+    async with engine.begin() as conn:
+        # Create table
+        await conn.execute(text("""
+            CREATE TABLE IF NOT EXISTS playing_with_neon (
+                id SERIAL PRIMARY KEY,
+                name TEXT NOT NULL,
+                value REAL
+            )
+        """))
+
+        # Insert data
+        await conn.execute(text("""
+            INSERT INTO playing_with_neon(name, value)
+            SELECT LEFT(md5(i::TEXT), 10), random()
+            FROM generate_series(1, 10) s(i)
+        """))
+
+        # Query data
+        result = await conn.execute(
+            text("SELECT * FROM playing_with_neon")
+        )
+
+        rows = result.fetchall()
+        for row in rows:
+            print(row)
+
+    await engine.dispose()
+
+asyncio.run(test_db())
